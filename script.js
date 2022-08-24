61dcf7fc796bcd996cb268f3f690a52ea7d6faf1
oscariotes <larrysuico2@gmail.com>
2022-08-24 15:38:36 +0800

    add event listener

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+const author = document.getElementById('author');
+const twitter = document.getElementById('twitter');
+const quote_button = document.getElementById('quote_button');
+
 /* let apiQuote = [];
 
 // Get quotes from API
@@ -19,7 +25,31 @@ getQuote(); */
 //get quotes from local array
 function newQoute() {
    const quote = localQuotes [Math.floor(Math.random() * localQuotes.length)];
-   console.log(quote);
+//if author is null
+   if (!quote.author) {
+      author.textContent = 'Unknown'
+   }else {
+      author.textContent = quote.author;
+   }
+ //for long quotes
+   if (quote.quote.length > 90) {
+      text.classList.add('long-text');
+   }else {
+      text.classList.remove('long-text');
+   }  
+   text.textContent = quote.quote; 
 }
+
+//tweet this
+
+function newTweet() {
+   const twitterUrl = `https://twitter.com/intent/tweet?text${text.textContent} - ${author.textContent}`;
+   window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+
+twitter.addEventListener('click', newTweet);
+quote_button.addEventListener('click', newQoute);
 
 newQoute();
